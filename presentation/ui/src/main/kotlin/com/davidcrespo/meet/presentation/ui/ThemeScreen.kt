b7cb17b2db84571466c143c45a9f42f433318fbf
David <david@A224.local>
2025-09-28 14:11:35 +0200

    use custom button

--- presentation/ui/src/main/kotlin/com/davidcrespo/meet/presentation/ui/ThemeScreen.kt
+++ presentation/ui/src/main/kotlin/com/davidcrespo/meet/presentation/ui/ThemeScreen.kt
@@ -3,7 +3,6 @@ package com.davidcrespo.meet.presentation.ui
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -11,8 +10,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.davidcrespo.domain.models.theme.ThemeMode
+import com.davidcrespo.meet.core.ui.Button
+import com.davidcrespo.meet.core.ui.auxiliary.ButtonStyle
 import com.davidcrespo.meet.presentation.ui.theme.MeetTheme
 import com.davidcrespo.meet.presentation.viewmodels.SettingsViewModel
 import org.koin.compose.viewmodel.koinViewModel
@@ -34,16 +36,18 @@ fun ThemeScreen(
             ) {
                 Text("THEME")
                 Button(
+                    text = "Toogle Theme",
                     onClick = {
                         when (theme) {
                             ThemeMode.LIGHT -> viewModel.setTheme(ThemeMode.DARK)
                             ThemeMode.DARK -> viewModel.setTheme(ThemeMode.SYSTEM)
                             ThemeMode.SYSTEM -> viewModel.setTheme(ThemeMode.LIGHT)
                         }
-                    }
-                ) {
-                    Text("Toggle Theme")
-                }
+                    },
+                    buttonStyle = ButtonStyle.PRIMARY,
+                    buttonModifier = Modifier.padding(16.dp),
+                    contentModifier = Modifier.padding(16.dp)
+                )
             }
         }
     }
